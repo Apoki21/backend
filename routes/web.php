05209fb8b53a92ff6dc1df331b68ff8ce43cd569
Apoki21 <a.nabilmudzakki@gmail.com>
2020-11-19 00:58:34 +0700

    main

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\DashboardController;
+use \App\Http\Controllers\ProductController;
+use \App\Http\Controllers\ProductGalleryController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+
+Auth::routes(['register'=> true]);
+
+Route::get('products/{id}/gallery', [ProductController::class, 'gallery'])->name('products.gallery');
+Route::resource('products', ProductController::class);
+
+Route::resource('product-galleries', ProductGalleryController::class);
